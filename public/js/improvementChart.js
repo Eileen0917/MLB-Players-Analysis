d8f0ebe4303ba91ce206722555b0e69aec30e4a1
Eileen Kuo <j405307@gmail.com>
2019-11-30 21:53:29 -0500

    finish query3

--- public/js/improvementChart.js
+++ public/js/improvementChart.js
@@ -1,25 +1,62 @@
 axios
   .get("/getImprovement")
   .then(function(response) {
-    let data = JSON.parse(response.data);
-    // let labels = Object.keys(data);
-    // let datas = Object.values(data);
-    // console.log(labels[0]);
-    // console.log(datas[0]);
+    const data = JSON.parse(response.data);
+    const h1 = data.h1;
+    const so1 = data.so1;
+    const era1 = data.era1;
+    const h2 = data.h2;
+    const so2 = data.so2;
+    const era2 = data.era2;
     console.log(data);
-    let aa = [
-      { x: 1, y: 2 },
-      { x: 2, y: 3 },
-      { x: 6, y: 5 }
-    ];
-    let ctx = document.getElementById("iChart");
+
+    let ctx = document.getElementById("iChart1");
+    let ctx2 = document.getElementById("iChart2");
+    let ctx3 = document.getElementById("iChart3");
+    let ctx4 = document.getElementById("iChart4");
+
     let myChart = new Chart(ctx, {
       type: "line",
       data: {
         datasets: [
           {
-            label: "ii",
-            data: aa
+            label: "H",
+            data: h1,
+            fill: false,
+            borderColor: "#e8c3b9"
+          },
+          {
+            label: "SO",
+            data: so1,
+            fill: false,
+            borderColor: "#aac399"
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: "linear"
+            }
+          ]
+        },
+        title: {
+          display: true,
+          text: "Max Scherzer"
+        }
+      }
+    });
+
+    let myChart2 = new Chart(ctx2, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            label: "ERA",
+            data: era1,
+            fill: false,
+            borderColor: "#003E3E"
           }
         ]
       },
@@ -33,18 +70,63 @@ axios
         }
       }
     });
-    // addData(myChart, labels, datas);
+
+    let myChart3 = new Chart(ctx3, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            label: "H",
+            data: h2,
+            fill: false,
+            borderColor: "#BB3D00"
+          },
+          {
+            label: "SO",
+            data: so2,
+            fill: false,
+            borderColor: "#3D7878"
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: "linear"
+            }
+          ]
+        },
+        title: {
+          display: true,
+          text: "Chien-Ming Wang"
+        }
+      }
+    });
+
+    let myChart4 = new Chart(ctx4, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            label: "ERA",
+            data: era2,
+            fill: false,
+            borderColor: "#003060"
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: "linear"
+            }
+          ]
+        }
+      }
+    });
   })
   .catch(function(error) {
     console.log(error);
   });
-
-function addData(chart, label, data) {
-  chart.data.labels.push(label);
-  chart.data.datasets.forEach(dataset => {
-    dataset.data.push(data);
-    console.log(data);
-  });
-  chart.update();
-  console.log("added");
-}
--- src/improvement.js
+++ src/improvement.js
@@ -1,37 +1,87 @@
 const db = require("./db");
-const sql = `SELECT P.nameFirst, P.nameLast, B.yearid, T.name, B.g, B.ab, B.r, B.h, B.rbi, B.hbp
-FROM HSLiao.Batting B
-    LEFT JOIN HSLiao.Teams T on B.teamID = T.teamID AND B.yearID = T.yearID
-    LEFT JOIN HSLiao.Players P ON P.playerID = B.playerID
-WHERE B.playerID in (
-    SELECT playerID
-    FROM HSLiao.Players
-    WHERE EXTRACT(YEAR FROM finalgame) - EXTRACT(YEAR FROM debut) >= 10)
-    AND B.playerID in (
-    SELECT playerID
-    FROM HSLiao.Batting
-    GROUP BY playerID
-    HAVING SUM(AB) > 1000
-    )
-ORDER BY B.playerID, B.yearID`;
+const q1 = `SELECT P1.nameFirst, P1.nameLast, P.yearID,
+p.h, P.so, P.era
+FROM HSLiao.Pitching P
+LEFT JOIN HSLiao.Teams T on P.teamID = T.teamID AND P.yearID = T.yearID
+LEFT JOIN HSLiao.Players P1 ON P1.playerID = P.playerID
+WHERE P.playerID in (
+SELECT playerID
+FROM HSLiao.Pitching
+GROUP BY playerID
+HAVING SUM(G) > 50
+INTERSECT
+SELECT playerID
+FROM HSLiao.Players
+WHERE EXTRACT(YEAR FROM finalgame) - EXTRACT(YEAR FROM debut) >= 10) and namelast = 'Scherzer'
+ORDER BY P.playerID, P.yearID`;
+
+const q2 = `SELECT P1.nameFirst, P1.nameLast, P.yearID, T.name, P.w, P.l, P.g, P.ipouts,
+p.h, P.er, P.hr, P.bb, P.so, P.era, P.r
+FROM HSLiao.Pitching P
+LEFT JOIN HSLiao.Teams T on P.teamID = T.teamID AND P.yearID = T.yearID
+LEFT JOIN HSLiao.Players P1 ON P1.playerID = P.playerID
+WHERE P.playerID in (
+SELECT playerID
+FROM HSLiao.Pitching
+GROUP BY playerID
+HAVING SUM(G) > 50
+INTERSECT
+SELECT playerID
+FROM HSLiao.Players
+WHERE EXTRACT(YEAR FROM finalgame) - EXTRACT(YEAR FROM debut) >= 10) and namelast = 'Wang'
+ORDER BY P.playerID, P.yearID`;
 
 async function run() {
-  const results = await db.run(sql);
-  //   let new_result = {};
-  //   for (const item of results) {
-  //     let log = {
-  //       x: item.WIN_RATE,
-  //       y: item.HIGH_SALARY_PLAYERS
-  //     };
-  //     if (!new_result[item.NAME]) {
-  //       new_result[item.NAME] = [];
-  //     }
-  //     new_result[item.NAME].push(log);
-  //   }
+  const r1 = await db.run(q1);
+  const h1 = [];
+  const so1 = [];
+  const era1 = [];
 
-  //   console.log(new_result);
+  for (const item of r1) {
+    let hLog = {
+      x: item.YEARID,
+      y: item.H
+    };
+    let soLog = {
+      x: item.YEARID,
+      y: item.SO
+    };
+    let eraLog = {
+      x: item.YEARID,
+      y: item.ERA
+    };
+    h1.push(hLog);
+    so1.push(soLog);
+    era1.push(eraLog);
+  }
 
-  return JSON.stringify(results);
+  const r2 = await db.run(q2);
+  const h2 = [];
+  const so2 = [];
+  const era2 = [];
+
+  for (const item of r2) {
+    let hLog = {
+      x: item.YEARID,
+      y: item.H
+    };
+    let soLog = {
+      x: item.YEARID,
+      y: item.SO
+    };
+    let eraLog = {
+      x: item.YEARID,
+      y: item.ERA
+    };
+    h2.push(hLog);
+    so2.push(soLog);
+    era2.push(eraLog);
+  }
+
+  console.log(r2);
+  const new_result = { h1, so1, era1, h2, so2, era2 };
+
+  return JSON.stringify(new_result);
 }
 
 module.exports = { run };
